13ee660166dd56a1c494057e8615650589b40e70
thediogen <dvodnenkoevgen1@gmail.com>
2025-01-03 13:34:13 +0200

    Added ability to read files

--- app/main.py
+++ app/main.py
@@ -5,3 +5,6 @@ print(f'Your name is {name} and you are {age} y.o.')
 
 skin_color = input('Enter your skin color: ')
 print(f'Your skin color is {skin_color}')
+
+with open('text.txt', 'r') as text:
+    print(text)
