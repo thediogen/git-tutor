8d8268ebbb6fef10681eb6840d9c0a50f76b4bba
thediogen <dvodnenkoevgen1@gmail.com>
2025-01-03 12:21:40 +0200

    Feature changes

--- app/main.py
+++ app/main.py
@@ -2,3 +2,6 @@ name = input('Enter your name: ')
 age = int(input('Enter yout age: '))
 
 print(f'Your name is {name} and you are {age} y.o.')
+
+skin_color = input('Enter your skin color: ')
+print(f'Your skin color is {skin_color}')
